8ae1f987df28c64a8165a069f3f4274586236e4c
chunsg0922 <74582719+chunsg0922@users.noreply.github.com>
2021-11-03 16:03:00 +0900

    1103로그

--- js/login.js
+++ js/login.js
@@ -37,6 +37,7 @@ function login(){
             },
             success: function(msg){
                 console.log('체성분 데이터 로딩 성공');
+                console.log(msg.bodyCompositions);
 
                 if(msg.bodyCompositions.length == 0){
                     alert('측정 데이터가 존재하지 않습니다.');
