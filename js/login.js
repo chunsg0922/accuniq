9b55a4afe0f644fb1f5e5aaf87f29d813b54bc55
chunsg0922 <74582719+chunsg0922@users.noreply.github.com>
2021-11-04 09:27:19 +0900

    1104 커밋

--- js/login.js
+++ js/login.js
@@ -27,7 +27,7 @@ function login(){
         sessionStorage.setItem("login", JSON.stringify(pw));
         
         $.ajax({
-            url: 'https://bca-proxy.accuniq.com/bodyComposition/find?query={%22where%22:{%22owner%22:' + user._id + '}}',
+            url: 'https://bca-proxy.accuniq.com/bodyComposition/find?query={%22where%22:{%22owner%22:' + user._id + '}, "sort":{"_id":-1}, "limit":1}',
             type: 'GET',
             dataType: 'JSON',
             async: false,
